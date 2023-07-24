64b6a2e3281af7653a2836fd84299d2eec9c74c4
Luciano <125805986+LucianoVelasquez@users.noreply.github.com>
2023-07-24 11:03:31 -0300

    Update loginGoogleController.js

--- src/controllers/loginGoogleController.js
+++ src/controllers/loginGoogleController.js
@@ -7,7 +7,7 @@ const postLoginGoogle = async({email, googleId, name}) =>{
     const user = await User.findOrCreate({ 
         where: { email },
         defaults: {
-            name: name.toLowerCase(),
+            name: name,
             email,
             password: googleId,
         } 
